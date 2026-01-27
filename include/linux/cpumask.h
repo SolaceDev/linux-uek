b6e5d146797ff61356b052f004bf2dd08b6de137
Robert Wang <robert.wang@solace.com>
2026-01-26 19:21:16 -0500

    Revert "Solace/Topology: Solace has some code that cannot support more then 32 codes for the thread affinity map, but don't limit if the system has more then 32 cores"
    This reverts commit 99a2616a39138bd0a38d16838615e8f70d1b687d.

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -1220,8 +1220,7 @@ static inline ssize_t
 cpumap_print_to_pagebuf_32bit(bool list, char *buf, const struct cpumask *mask)
 {
 	return bitmap_print_to_pagebuf(list, buf, cpumask_bits(mask),
-				       (*cpumask_bits(mask) > 31U) ? nr_cpu_ids: 
-                                        min(nr_cpu_ids,32U));
+				      min(nr_cpu_ids,32U));
 }
 
 /**
